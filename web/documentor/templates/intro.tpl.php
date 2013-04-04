8f2d1765a174bf17fa55396087612af9a20af4cc
Daniel Spors <daniels@scavix.com>
2013-04-04 15:07:24 +0200

    Corrected some names

--- web/documentor/templates/intro.tpl.php
+++ web/documentor/templates/intro.tpl.php
@@ -1,8 +1,8 @@
-<h1>Scavix WDF Documentor</h1>
+<h1>WebFramework Documentor</h1>
 <p>
-	This is a tool to create documentation from DocComments in the scavix-wdf sourcecode.
+	This is a tool to create documentation from DocComments in the WebFramework sourcecode.
 	We use it to automate and speed up things - and it will constantly remind us of missing documentation.
-	It will create a bunch of cross-linked markdown files that we push to the scvavix-wdf.wiki repository
+	It will create a bunch of cross-linked markdown files that we push to the WebFramework.wiki repository
 	thus using the standard GitHub mechanism for our documentation.
 </p>
 <div style="text-align: center; font-size: 18px; font-weight: bold"><?=$run?></div>
