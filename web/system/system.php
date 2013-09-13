b260a1d4b8397aa3951b9d351a01e030684f918f
Daniel Spors <daniels@scavix.com>
2013-09-13 14:43:50 +0200

    Added ExternalResourceAttribute to known full qualified classnames
    Signed-off-by: Daniel Spors <daniels@scavix.com>

--- web/system/system.php
+++ web/system/system.php
@@ -1491,18 +1491,19 @@ function fq_class_name($classname)
 	$cnl = strtolower($classname);
 	switch( $cnl )
 	{
-		case 'template':              return '\\ScavixWDF\\Base\\Template';
-		case 'renderable':            return '\\ScavixWDF\\Base\\Renderable';
-		case 'control':               return '\\ScavixWDF\\Base\\Control';
-		case 'requestparamattribute': return '\\ScavixWDF\\Reflection\\RequestParamAttribute';
-		case 'resourceattribute':     return '\\ScavixWDF\\Reflection\\ResourceAttribute';
-		case 'nominifyattribute':     return '\\ScavixWDF\\Reflection\\NoMinifyAttribute';
-		case 'wdfresource':           return '\\ScavixWDF\\WdfResource';
-		case 'datasource':            return '\\ScavixWDF\\Model\\DataSource';
-		case 'sysadmin':              return '\\ScavixWDF\\Admin\\SysAdmin';
-		case 'minifyadmin':           return '\\ScavixWDF\\Admin\\MinifyAdmin';
-		case 'tracelogger':           return '\\ScavixWDF\\Logging\\TraceLogger';
-		case 'phpsession':            return '\\ScavixWDF\\Session\\PhpSession';
+		case 'template':                  return '\\ScavixWDF\\Base\\Template';
+		case 'renderable':                return '\\ScavixWDF\\Base\\Renderable';
+		case 'control':                   return '\\ScavixWDF\\Base\\Control';
+		case 'requestparamattribute':     return '\\ScavixWDF\\Reflection\\RequestParamAttribute';
+		case 'resourceattribute':         return '\\ScavixWDF\\Reflection\\ResourceAttribute';
+		case 'externalresourceattribute': return '\\ScavixWDF\\Reflection\\ExternalResourceAttribute';
+		case 'nominifyattribute':         return '\\ScavixWDF\\Reflection\\NoMinifyAttribute';
+		case 'wdfresource':               return '\\ScavixWDF\\WdfResource';
+		case 'datasource':                return '\\ScavixWDF\\Model\\DataSource';
+		case 'sysadmin':                  return '\\ScavixWDF\\Admin\\SysAdmin';
+		case 'minifyadmin':               return '\\ScavixWDF\\Admin\\MinifyAdmin';
+		case 'tracelogger':               return '\\ScavixWDF\\Logging\\TraceLogger';
+		case 'phpsession':                return '\\ScavixWDF\\Session\\PhpSession';
 	}
 	
 	if( isset($GLOBALS['system_class_alias'][$cnl]) )
