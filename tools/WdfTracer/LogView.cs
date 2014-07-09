1180b780e123eb559baaf038af40db2241a0c4f1
Maximilin Stever <maximilians@scavix.com>
2014-07-09 13:26:10 +0200

    Working SWDF-21: added month for regex

--- tools/WdfTracer/LogView.cs
+++ tools/WdfTracer/LogView.cs
@@ -507,18 +507,32 @@ namespace WdfTracer
             {
                 case "jan": return 1;
                 case "feb": return 2;
-                case "mar": return 3;
+                case "mar":
+                case "märz":
+                            return 3;
                 case "apr": return 4;
-                case "may": return 5;
-                case "jun": return 6;
-                case "jul": return 7;
+                case "may":
+                case "mai":    
+                            return 5;
+                case "jun":
+                case "june":
+                case "juni":
+                            return 6;
+                case "jul":
+                case "july":
+                case "juli":
+                            return 7;
                 case "aug": return 8;
-                case "sep": return 9;
+                case "sep":
+                case "sept":
+                            return 9;
                 case "oct":
                 case "okt":
                     return 10;
                 case "nov": return 11;
-                case "dec": return 12;
+                case "dec":
+                case "dez":
+                           return 12;
             }
             return int.Parse(month);
         }
